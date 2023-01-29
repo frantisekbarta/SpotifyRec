1d12d2a9b64971a3d6a06e3b56b2f5841968c490
frantisekbarta <88767131+frantisekbarta@users.noreply.github.com>
2023-01-29 17:54:10 +0100

    Update RelayCommand.cs

--- SpotifyRec/RelayCommand.cs
+++ SpotifyRec/RelayCommand.cs
@@ -8,17 +8,17 @@ namespace SpotifyRec
         readonly Action<object> execute;
         readonly Predicate<object> canExecute;
 
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+
+        }
+
         public RelayCommand(Action<object> execute, Predicate<object> canExecute)
         {
             if (execute == null)
                 throw new ArgumentNullException("execute");
             this.execute = execute;
             this.canExecute = canExecute;
-        }
-
-        public RelayCommand(Action<object> execute) : this(execute, null)
-        {
-
         }
 
         public event EventHandler CanExecuteChanged
